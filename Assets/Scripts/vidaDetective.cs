094307bce1d2c396ad2823c5351347d77c7accf7
Torrente_ <carlos.tenorio.14.81.33@gmail.com>
2024-09-21 20:59:03 -0600

    Actualización [21-09-24] - Solución al dibujado de la vida

--- Assets/Scripts/vidaDetective.cs
+++ Assets/Scripts/vidaDetective.cs
@@ -10,48 +10,52 @@ public class vidaDetective : MonoBehaviour
     public Image[] corazones;
     public Sprite corazonCompleto;
     public Sprite corazonMitad;
+    public Sprite corazonVacio;
+
+    private int vidaReal;
     public static double vida;
     private bool haMuerto;
+    public GameObject gameOver;
     public static int puedePerderVida = 1;
 
     void Start() {
         haMuerto = false;
-        vida = 20;
-        //gameOver.SetActive(false);
+        vida = 10;
+        //Este código hace que el jugador tenga 20 puntos de vida, que se representan como 10 corazones completos.
         corazones = Vida.GetComponentsInChildren<Image>();
-
-
+        gameOver.SetActive(false);
     }
 
     void Update() {
-    }
-
-    public void tomarDaño(double Daño) {
-        if(vida >= .5 && puedePerderVida == 1) {
-            puedePerderVida = 0;
-            vida -= Daño;
-            actualizarCorazones();
-            Debug.Log("Vida: " + vida);
-        }
+        actualizarCorazones();
         if(vida <= 0 && !haMuerto) {
             haMuerto = true;
             StartCoroutine(EjecutaMuerte());
         }
     }
 
-    void actualizarCorazones() { //Arreglar. No actualiza los corazones.
-        //Debug.Log("Actualizando uwu");
+    public void tomarDaño(double Daño) {
+        if(vida >= .5 && puedePerderVida == 1) {
+            puedePerderVida = 0;
+            vida -= Daño;
+        }
+        Debug.Log("Vida: " + vida);
+    }
+
+    void actualizarCorazones() {
         for(int i = 0; i < corazones.Length; i++){
             if(vida >= i + 1){
                 corazones[i].sprite = corazonCompleto;
             } else if(vida >= i + 0.5f) {
                 corazones[i].sprite = corazonMitad;
+            } else {
+                corazones[i].sprite = corazonVacio;
             }
         }
     }
 
     IEnumerator EjecutaMuerte() {
-        yield return new WaitForSeconds(1.5f);
-        //gameOver.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        gameOver.SetActive(true);
     }
 }
